fbc8f33522a962e56540ba3e67dd633a13ff6082
Ani-110ani <ani6asatiani@gmail.com>
2026-08-11 13:15:39 +0400

    add img tag in js

---
+++ app.js
@@ -0,0 +1,6 @@
+import { results } from "./data.js";
+
+const img = document.createElement("img");
+img.src =
+  "https://fastly.picsum.photos/id/534/1000/800.jpg?hmac=tFbU1nZ2RnQNroI_ToBhH-LFB8KNjyWoc3bVv5G9Wkw";
+document.body.appendChild(img);
