23749a9c61680388e403a3dc4aebc82718429c2a
Ani-110ani <ani6asatiani@gmail.com>
2026-08-11 13:21:45 +0400

    map flight data to generate html cards

--- app.js
+++ app.js
@@ -4,3 +4,28 @@ const img = document.createElement("img");
 img.src =
   "https://fastly.picsum.photos/id/534/1000/800.jpg?hmac=tFbU1nZ2RnQNroI_ToBhH-LFB8KNjyWoc3bVv5G9Wkw";
 document.body.appendChild(img);
+
+const flightList = document.getElementById("flight-list");
+
+const cardsHtml = results
+  .map((item) => {
+    const isDirect = item.content.flightRoutes?.directFlightsAvailable
+      ? "Direct"
+      : "Indirect";
+    const price = item.content.flightQuotes?.cheapest?.price || "N/A";
+
+    return `
+        <div class="card">
+            <img src="${item.content.image.url}" alt="${item.content.location.name}">
+            <div class="card-info">
+                <span>${item.content.location.name}</span>
+                <span class="flight-details" style="display: none;">${isDirect},${price}</span>
+            </div>
+            <div class="buttons">
+                <button class="see-more">See more details</button>
+                <button class="remove-card">Remove Flight</button>
+            </div>
+        </div>
+    `;
+  })
+  .join("");
